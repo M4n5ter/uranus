7a517c46afc63f3c8422a8118cf0810942e5fdc7
王勇涛 <wangyongtao2000@gmail.com>
2022-03-30 19:59:54 +0800

    update order.model.vars

--- app/order/model/vars.go
+++ app/order/model/vars.go
@@ -3,3 +3,21 @@ package model
 import "github.com/zeromicro/go-zero/core/stores/sqlx"
 
 var ErrNotFound = sqlx.ErrNotFound
+
+// FlightOrderTradeStateCancel 订单取消
+var FlightOrderTradeStateCancel int64 = -1
+
+// FlightOrderTradeStateWaitPay 等待支付
+var FlightOrderTradeStateWaitPay int64 = 0
+
+// FlightOrderTradeStateWaitUse 等待使用
+var FlightOrderTradeStateWaitUse int64 = 1
+
+// FlightOrderTradeStateUsed 已使用
+var FlightOrderTradeStateUsed int64 = 2
+
+// FlightOrderTradeStateRefund 退款
+var FlightOrderTradeStateRefund int64 = 3
+
+// FlightOrderTradeStateExpire 订单过期
+var FlightOrderTradeStateExpire int64 = 4
