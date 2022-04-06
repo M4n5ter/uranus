9471461f8b77efe22051a3fad56559fc7e8c3c40
çe <wangyongtao2000@qq.com>
2022-04-06 08:49:22 +0800

    完成航班预定api

--- app/flightReservation/cmd/api/internal/logic/flightReservation/flightReservationLogic.go
+++ app/flightReservation/cmd/api/internal/logic/flightReservation/flightReservationLogic.go
@@ -2,12 +2,21 @@ package flightReservation
 
 import (
 	"context"
+	"github.com/pkg/errors"
+	"google.golang.org/protobuf/types/known/timestamppb"
+	"uranus/app/flightReservation/cmd/rpc/flightreservation"
+	"uranus/common/ctxdata"
+	"uranus/common/timeTools"
+	"uranus/common/xerr"
 
 	"uranus/app/flightReservation/cmd/api/internal/svc"
 	"uranus/app/flightReservation/cmd/api/internal/types"
 
 	"github.com/zeromicro/go-zero/core/logx"
 )
+
+var ERRInvalidInput = xerr.NewErrMsg("非法输入")
+var ERRBookFail = xerr.NewErrMsg("预定机票失败")
 
 type FlightReservationLogic struct {
 	logx.Logger
@@ -24,7 +33,40 @@ func NewFlightReservationLogic(ctx context.Context, svcCtx *svc.ServiceContext) 
 }
 
 func (l *FlightReservationLogic) FlightReservation(req *types.FlightReservationReq) (resp *types.FlightReservationResp, err error) {
-	// todo: add your logic here and delete this line
+	// 检查输入
+	if len(req.FlightNumber) == 0 || len(req.DepartTime) == 0 || len(req.ArriveTime) == 0 || len(req.ArrivePosition) == 0 || len(req.DepartPosition) == 0 || len(req.SetOutDate) == 0 {
+		return nil, errors.Wrapf(ERRInvalidInput, "")
+	}
 
+	sod, err := timeTools.String2TimeYMD(req.SetOutDate)
+	if err != nil {
+		return nil, errors.Wrapf(ERRInvalidInput, "invalid input SOD err: %v", err)
+	}
+
+	arriveTime, err := timeTools.String2TimeYMDhms(req.ArriveTime)
+	if err != nil {
+		return nil, errors.Wrapf(ERRInvalidInput, "invalid input arriveTime err: %v", err)
+	}
+
+	departTime, err := timeTools.String2TimeYMDhms(req.DepartTime)
+	if err != nil {
+		return nil, errors.Wrapf(ERRInvalidInput, "invalid input departTime err: %v", err)
+	}
+
+	bookResp, err := l.svcCtx.FlightReservationRpcClient.BookAirTickets(l.ctx, &flightreservation.BookAirTicketsReq{
+		UserID:         ctxdata.GetUidFromCtx(l.ctx),
+		FlightNumber:   req.FlightNumber,
+		SetOutDate:     timestamppb.New(sod),
+		IsFirstClass:   req.IsFirstClass,
+		DepartPosition: req.DepartPosition,
+		DepartTime:     timestamppb.New(departTime),
+		ArrivePosition: req.ArrivePosition,
+		ArriveTime:     timestamppb.New(arriveTime),
+	})
+	if err != nil {
+		return nil, errors.Wrapf(ERRBookFail, "Rpc ERR in l.svcCtx.FlightReservationRpcClient.BookAirTickets: err: %v, req: %+v", err, req)
+	}
+
+	resp.OrderSn = bookResp.OrderSn
 	return
 }
--- app/flightReservation/cmd/api/internal/svc/serviceContext.go
+++ app/flightReservation/cmd/api/internal/svc/serviceContext.go
@@ -1,15 +1,19 @@
 package svc
 
 import (
+	"github.com/zeromicro/go-zero/zrpc"
 	"uranus/app/flightReservation/cmd/api/internal/config"
+	"uranus/app/flightReservation/cmd/rpc/flightreservation"
 )
 
 type ServiceContext struct {
-	Config config.Config
+	Config                     config.Config
+	FlightReservationRpcClient flightreservation.FlightReservation
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
 	return &ServiceContext{
-		Config: c,
+		Config:                     c,
+		FlightReservationRpcClient: flightreservation.NewFlightReservation(zrpc.MustNewClient(c.FlightReservation)),
 	}
 }
