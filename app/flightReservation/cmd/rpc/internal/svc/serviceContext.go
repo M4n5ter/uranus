3a3c4b800f2c7a0c1fcae34f3e0128c9acc2a10b
çe <wangyongtao2000@qq.com>
2022-04-12 10:28:32 +0800

    增加了 GetFee()

--- app/flightReservation/cmd/rpc/internal/svc/serviceContext.go
+++ app/flightReservation/cmd/rpc/internal/svc/serviceContext.go
@@ -3,9 +3,11 @@ package svc
 import (
 	"github.com/zeromicro/go-zero/core/stores/sqlx"
 	"github.com/zeromicro/go-zero/zrpc"
+	"time"
 	"uranus/app/flightInquiry/cmd/rpc/flightinquiry"
 	"uranus/app/flightReservation/cmd/rpc/internal/config"
 	"uranus/app/order/cmd/rpc/order"
+	"uranus/app/payment/cmd/rpc/payment"
 	"uranus/commonModel"
 )
 
@@ -18,6 +20,7 @@ type ServiceContext struct {
 	RefundAndChangeInfosModel commonModel.RefundAndChangeInfosModel
 	FlightInquiryRpcClient    flightinquiry.FlightInquiry
 	OrderRpcClient            order.Order
+	PaymentRpcClient          payment.Payment
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
@@ -30,5 +33,23 @@ func NewServiceContext(c config.Config) *ServiceContext {
 		RefundAndChangeInfosModel: commonModel.NewRefundAndChangeInfosModel(sqlx.NewMysql(c.DB.DataSource), c.Cache),
 		FlightInquiryRpcClient:    flightinquiry.NewFlightInquiry(zrpc.MustNewClient(c.FlightInquiryRpcConf)),
 		OrderRpcClient:            order.NewOrder(zrpc.MustNewClient(c.OrderRpcConf)),
+		PaymentRpcClient:          payment.NewPayment(zrpc.MustNewClient(c.PaymentRpcConf)),
 	}
+}
+
+// GetFee 获取退改票手续费
+func (s *ServiceContext) GetFee(rci *commonModel.RefundAndChangeInfos) (fee int64, ok bool) {
+	now := time.Now()
+	if now.Before(rci.Time1) {
+		return rci.Fee1, true
+	} else if now.Before(rci.Time2) {
+		return rci.Fee2, true
+	} else if now.Before(rci.Time3) {
+		return rci.Fee3, true
+	} else if now.Before(rci.Time4) {
+		return rci.Fee4, true
+	} else if now.Before(rci.Time5) {
+		return rci.Fee5, true
+	}
+	return 99999999999, false
 }
