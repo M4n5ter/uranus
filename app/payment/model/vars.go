5d49ff367499e901ada7543bdcdbd1ef2b3c767a
çe <wangyongtao2000@qq.com>
2022-04-12 10:29:28 +0800

    更正了部分值

--- app/payment/model/vars.go
+++ app/payment/model/vars.go
@@ -17,15 +17,15 @@ var (
 	PaymentLocalPayStatusFAIL    int64 = -1 //支付失败
 	PaymentLocalPayStatusWait    int64 = 0  //待支付
 	PaymentLocalPayStatusSuccess int64 = 1  //支付成功
-	PaymentLocalPayStatusRefund  int64 = 2  //已退款
+	PaymentLocalPayStatusRefund  int64 = 3  //已退款
 )
 
-// 第三方支付状态 0:未支付 1:支付成功 -1:支付失败 2:已退款
+// 第三方支付状态 0:未支付 1:支付成功 -1:支付失败 3:已退款
 var (
 	ThirdPartyPayTradeStateWait    int64 = 0
 	ThirdPartyPayTradeStateSuccess int64 = 1
 	ThirdPartyPayTradeStateFAIL    int64 = -1
-	ThirdPartyPayTradeStateRefund  int64 = 2
+	ThirdPartyPayTradeStateRefund  int64 = 3
 )
 
 // 通用支付状态
@@ -34,7 +34,7 @@ var (
 	CommonPayFAIL    int64 = -1 // 支付失败
 	CommonPaySuccess int64 = 1  // 支付成功
 	CommonPayWait    int64 = 0  // 待支付
-	CommonPayRefund  int64 = 2  // 已退款
+	CommonPayRefund  int64 = 3  // 已退款
 	CommonPayDiscard int64 = -2 // 回滚废弃
 )
 
