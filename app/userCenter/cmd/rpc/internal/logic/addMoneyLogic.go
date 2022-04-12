74a7790c991b178747fad18297291a69cf484eb7
çe <wangyongtao2000@qq.com>
2022-04-12 11:08:39 +0800

    接入 dtm

--- app/userCenter/cmd/rpc/internal/logic/addMoneyLogic.go
+++ app/userCenter/cmd/rpc/internal/logic/addMoneyLogic.go
@@ -2,7 +2,12 @@ package logic
 
 import (
 	"context"
+	"database/sql"
+	"github.com/dtm-labs/dtm/dtmgrpc"
 	"github.com/pkg/errors"
+	"github.com/zeromicro/go-zero/core/stores/sqlx"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 	"uranus/app/userCenter/model"
 	"uranus/common/xerr"
 
@@ -29,31 +34,43 @@ func NewAddMoneyLogic(ctx context.Context, svcCtx *svc.ServiceContext) *AddMoney
 func (l *AddMoneyLogic) AddMoney(in *pb.AddMoneyReq) (*pb.AddMoneyResp, error) {
 	// 检查输入
 	if in.Money < 0 {
-		return nil, errors.Wrapf(xerr.NewErrMsg("增加的钱不能小于 0 ，扣钱请使用隔壁rpc"), "")
+		return nil, status.Error(codes.Aborted, errors.Wrapf(xerr.NewErrMsg("增加的钱不能小于 0 ，扣钱请使用隔壁rpc"), "").Error())
 	}
 
 	// 检查用户是否存在
 	_, err := l.svcCtx.UserModel.FindOne(in.UserId)
 	if err != nil {
 		if err == model.ErrNotFound {
-			return nil, errors.Wrapf(xerr.NewErrMsg("用户不存在"), "Not Found userID: %d", in.UserId)
+			return nil, status.Error(codes.Aborted, errors.Wrapf(xerr.NewErrMsg("用户不存在"), "Not Found userID: %d", in.UserId).Error())
 		}
-		return nil, errors.Wrapf(xerr.NewErrCode(xerr.DB_ERROR), "DBERR: %v", err)
+		return nil, status.Error(codes.Aborted, errors.Wrapf(xerr.NewErrCode(xerr.DB_ERROR), "DBERR: %v", err).Error())
 	}
 	// 检查用户是否有钱包
 	wallet, err := l.svcCtx.WalletModel.FindOneByUserId(in.UserId)
 	if err != nil && err != model.ErrNotFound {
-		return nil, errors.Wrapf(xerr.NewErrCode(xerr.DB_ERROR), "DBERR: %v", err)
+		return nil, status.Error(codes.Aborted, errors.Wrapf(xerr.NewErrCode(xerr.DB_ERROR), "DBERR: %v", err).Error())
 	}
 
 	if wallet == nil {
-		return nil, errors.Wrapf(xerr.NewErrMsg("该用户没有钱包"), "userID: %d", in.UserId)
+		return nil, status.Error(codes.Aborted, errors.Wrapf(xerr.NewErrMsg("该用户没有钱包"), "userID: %d", in.UserId).Error())
 	}
-	// 加钱
-	wallet.Money = wallet.Money + in.Money
-	err = l.svcCtx.WalletModel.UpdateWithVersion(nil, wallet)
+
+	barrier, err := dtmgrpc.BarrierFromGrpc(l.ctx)
+	db, err := sqlx.NewMysql(l.svcCtx.Config.DB.DataSource).RawDB()
 	if err != nil {
-		return nil, errors.Wrapf(xerr.NewErrMsg("加钱失败"), "DBERR: %v", err)
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+	if err := barrier.CallWithDB(db, func(tx *sql.Tx) error {
+		// 加钱
+		wallet.Money = wallet.Money + in.Money
+		err = l.svcCtx.WalletModel.UpdateWithVersion(sqlx.NewSessionFromTx(tx), wallet)
+		if err != nil {
+			return errors.Wrapf(xerr.NewErrMsg("加钱失败"), "DBERR: %v", err)
+		}
+
+		return nil
+	}); err != nil {
+		return nil, status.Error(codes.Aborted, err.Error())
 	}
 
 	return &pb.AddMoneyResp{}, nil
