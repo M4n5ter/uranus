72eeeb108563337391e6f26dc3485e9fc71eb298
çe <wangyongtao2000@qq.com>
2022-04-15 15:01:37 +0800

    使用goctl1.3.4

--- app/flightInquiry/cmd/api/internal/handler/flightInquiry/quireBySetOutDateAndFlightNumberHandler.go
+++ app/flightInquiry/cmd/api/internal/handler/flightInquiry/quireBySetOutDateAndFlightNumberHandler.go
@@ -3,23 +3,23 @@ package flightInquiry
 import (
 	"net/http"
 
-	"uranus/app/flightInquiry/cmd/api/internal/logic/flightInquiry"
-	"uranus/app/flightInquiry/cmd/api/internal/svc"
-	"uranus/app/flightInquiry/cmd/api/internal/types"
 	"uranus/common/result"
 
 	"github.com/zeromicro/go-zero/rest/httpx"
+	"uranus/app/flightInquiry/cmd/api/internal/logic/flightInquiry"
+	"uranus/app/flightInquiry/cmd/api/internal/svc"
+	"uranus/app/flightInquiry/cmd/api/internal/types"
 )
 
-func QuireBySetOutDateAndFlightNumberHandler(ctx *svc.ServiceContext) http.HandlerFunc {
+func QuireBySetOutDateAndFlightNumberHandler(svcCtx *svc.ServiceContext) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var req types.QuireBySetOutDateAndFlightNumberReq
 		if err := httpx.Parse(r, &req); err != nil {
-			httpx.Error(w, err)
+			result.ParamErrorResult(r, w, err)
 			return
 		}
 
-		l := flightInquiry.NewQuireBySetOutDateAndFlightNumberLogic(r.Context(), ctx)
+		l := flightInquiry.NewQuireBySetOutDateAndFlightNumberLogic(r.Context(), svcCtx)
 		resp, err := l.QuireBySetOutDateAndFlightNumber(&req)
 		result.HttpResult(r, w, resp, err)
 	}
--- app/flightInquiry/cmd/api/internal/handler/flightInquiry/quireBySetOutDateStartPositionEndPositionHandler.go
+++ app/flightInquiry/cmd/api/internal/handler/flightInquiry/quireBySetOutDateStartPositionEndPositionHandler.go
@@ -3,23 +3,23 @@ package flightInquiry
 import (
 	"net/http"
 
-	"uranus/app/flightInquiry/cmd/api/internal/logic/flightInquiry"
-	"uranus/app/flightInquiry/cmd/api/internal/svc"
-	"uranus/app/flightInquiry/cmd/api/internal/types"
 	"uranus/common/result"
 
 	"github.com/zeromicro/go-zero/rest/httpx"
+	"uranus/app/flightInquiry/cmd/api/internal/logic/flightInquiry"
+	"uranus/app/flightInquiry/cmd/api/internal/svc"
+	"uranus/app/flightInquiry/cmd/api/internal/types"
 )
 
-func QuireBySetOutDateStartPositionEndPositionHandler(ctx *svc.ServiceContext) http.HandlerFunc {
+func QuireBySetOutDateStartPositionEndPositionHandler(svcCtx *svc.ServiceContext) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var req types.QuireBySetOutDateStartPositionEndPositionReq
 		if err := httpx.Parse(r, &req); err != nil {
-			httpx.Error(w, err)
+			result.ParamErrorResult(r, w, err)
 			return
 		}
 
-		l := flightInquiry.NewQuireBySetOutDateStartPositionEndPositionLogic(r.Context(), ctx)
+		l := flightInquiry.NewQuireBySetOutDateStartPositionEndPositionLogic(r.Context(), svcCtx)
 		resp, err := l.QuireBySetOutDateStartPositionEndPosition(&req)
 		result.HttpResult(r, w, resp, err)
 	}
