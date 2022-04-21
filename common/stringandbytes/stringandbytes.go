c69572623ade922b4d5f1a0aa3a9093c83ba4695
M4n5ter <wangyongtao2000@qq.com>
2022-04-21 21:52:07 +0800

    update

--- common/stringandbytes/stringandbytes.go
+++ common/stringandbytes/stringandbytes.go
@@ -5,7 +5,8 @@ import (
 	"unsafe"
 )
 
-// String2Bytes 将 string 强转换为 []byte ，性能高于标准转换 []byte(string),但可能造成危险(如对转换后的 []byte 操作会导致对原本不能被修改的 string 被修改,并且不能被 defer + recover 捕获错误)
+// String2Bytes 将 string 强转换为 []byte ，性能高于标准转换 []byte(string),
+//但可能造成危险(如对转换后的 []byte 操作会导致对原本不能被修改的 string 被修改,并且不能被 defer + recover 捕获错误)
 func String2Bytes(s string) []byte {
 	sh := (*reflect.StringHeader)(unsafe.Pointer(&s))
 	bh := reflect.SliceHeader{
