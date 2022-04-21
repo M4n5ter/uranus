59dd8d94b8a88d1d2b2f223c2a73789c1a65b2c7
M4n5ter <wangyongtao2000@qq.com>
2022-04-21 21:50:48 +0800

    update

--- app/flightInquiry/bizcache/bizcache.go
+++ app/flightInquiry/bizcache/bizcache.go
@@ -1,56 +1,112 @@
 package bizcache
 
 import (
+	"fmt"
+	"github.com/zeromicro/go-zero/core/hash"
+	"github.com/zeromicro/go-zero/core/jsonx"
 	"github.com/zeromicro/go-zero/core/stores/redis"
+	"strconv"
 	"time"
+	"uranus/common/stringandbytes"
 	"uranus/commonModel"
 )
 
+const BizFLICachePrefix = `biz:fli:cache:%s`
+const BizSpaceCachePrefix = `biz:space:cache:%s`
+
 type FLI commonModel.FlightInfos
 
-const bizContentCacheKey = `biz#fli#cache`
+// AddID 提供id存储
+func AddID(r redis.Redis, id int64, zset, prefix string) error {
+	bizFLICacheKey := fmt.Sprintf(prefix, zset)
+	// 过期时间为 12 小时
+	err := r.Expire(bizFLICacheKey, 43200)
+	if err != nil {
+		return err
+	}
 
-// AddFLI 提供航班内容存储
-func AddFLI(r redis.Redis, c *FLI) error {
-	v := compress(c)
-	_, err := r.Zadd(bizContentCacheKey, c.CreateTime.UnixNano()/1e6, v)
+	_, err = r.Zadd(bizFLICacheKey, time.Now().UnixNano()/1e6, strconv.Itoa(int(id)))
 	return err
 }
 
-// DelFLI 提供内容删除
-func DelFLI(r redis.Redis, c *FLI) error {
-	v := compress(c)
-	_, err := r.Zrem(bizContentCacheKey, v)
-
+// DelID 提供id删除
+func DelID(r redis.Redis, id int64, zset, prefix string) error {
+	bizFLICacheKey := fmt.Sprintf(prefix, zset)
+	_, err := r.Zrem(bizFLICacheKey, strconv.Itoa(int(id)))
 	return err
 }
 
-// 内容压缩
-func compress(c *FLI) string {
-	// todo
-	var ret string
-	return ret
+// 航班内容压缩
+func compress(f *FLI) (string, error) {
+	data, err := f.Marshal()
+	if err != nil {
+		return "", err
+	}
+
+	return stringandbytes.Bytes2String(data), nil
 }
 
-// 内容解压
-func unCompress(v string) *FLI {
-	// todo
-	var ret FLI
-	return &ret
-}
-
-// ListByRangeTime 提供根据时间段进行数据查询
-func ListByRangeTime(r redis.Redis, start, end time.Time) ([]*FLI, error) {
-	kvs, err := r.ZrangebyscoreWithScores(bizContentCacheKey, start.UnixNano()/1e6, end.UnixNano()/1e6)
+// 航班内容解压
+func unCompress(v string) (*FLI, error) {
+	data := stringandbytes.String2Bytes(v)
+	var ret *FLI
+	ret, err := Unmarshal(data)
 	if err != nil {
 		return nil, err
 	}
 
-	var list []*FLI
+	return ret, nil
+}
+
+// ListByRangeTime 提供根据时间段进行数据查询
+func ListByRangeTime(r redis.Redis, zset, prefix string, start, end time.Time) ([]int64, error) {
+	bizFLICacheKey := fmt.Sprintf(prefix, zset)
+	kvs, err := r.ZrangebyscoreWithScores(bizFLICacheKey, start.UnixNano()/1e6, end.UnixNano()/1e6)
+	if err != nil {
+		return nil, err
+	}
+
+	var list []int64
 	for _, kv := range kvs {
-		data := unCompress(kv.Key)
-		list = append(list, data)
+		id, _ := strconv.ParseInt(kv.Key, 10, 64)
+		list = append(list, id)
 	}
 
 	return list, nil
+}
+
+func ListAll(r redis.Redis, zset, prefix string) ([]int64, error) {
+	bizFLICacheKey := fmt.Sprintf(prefix, zset)
+	stringList, err := r.Zrange(bizFLICacheKey, 0, -1)
+	if err != nil {
+		return nil, err
+	}
+
+	var list []int64
+	for _, s := range stringList {
+		id, _ := strconv.ParseInt(s, 10, 64)
+		list = append(list, id)
+	}
+
+	return list, nil
+}
+
+func (f *FLI) Marshal() ([]byte, error) {
+	return jsonx.Marshal(f)
+}
+
+func Unmarshal(data []byte) (*FLI, error) {
+	var fli FLI
+	err := jsonx.Unmarshal(data, &fli)
+	if err != nil {
+		return nil, err
+	}
+
+	return &fli, nil
+}
+
+// String2Score 字符串转换成 score
+func String2Score(s string) int64 {
+	data := stringandbytes.String2Bytes(s)
+	return int64(hash.Hash(data) / 2)
 }
