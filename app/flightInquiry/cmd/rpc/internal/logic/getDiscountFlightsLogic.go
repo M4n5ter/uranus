fb5e64073ef83d37eb498617c67afc1da18093ac
M4n5ter <wangyongtao2000@qq.com>
2022-05-13 16:24:55 +0800

    查询折扣航班

--- app/flightInquiry/cmd/rpc/internal/logic/getDiscountFlightsLogic.go
+++ app/flightInquiry/cmd/rpc/internal/logic/getDiscountFlightsLogic.go
@@ -2,6 +2,13 @@ package logic
 
 import (
 	"context"
+	"fmt"
+	"github.com/pkg/errors"
+	"sort"
+	"time"
+	"uranus/app/flightInquiry/bizcache"
+	"uranus/common/xerr"
+	"uranus/commonModel"
 
 	"uranus/app/flightInquiry/cmd/rpc/internal/svc"
 	"uranus/app/flightInquiry/cmd/rpc/pb"
@@ -15,6 +22,8 @@ type GetDiscountFlightsLogic struct {
 	logx.Logger
 }
 
+type CanBeSortedFLI []*pb.FlightInfo
+
 func NewGetDiscountFlightsLogic(ctx context.Context, svcCtx *svc.ServiceContext) *GetDiscountFlightsLogic {
 	return &GetDiscountFlightsLogic{
 		ctx:    ctx,
@@ -25,7 +34,79 @@ func NewGetDiscountFlightsLogic(ctx context.Context, svcCtx *svc.ServiceContext)
 
 // GetDiscountFlights 查询折扣航班
 func (l *GetDiscountFlightsLogic) GetDiscountFlights(in *pb.GetDiscountFlightsReq) (*pb.GetDiscountFlightsResp, error) {
-	// todo: add your logic here and delete this line
+	// 检查输入
+	if len(in.ArrivePosition) == 0 || len(in.DepartPosition) == 0 {
+		return nil, errors.Wrapf(xerr.NewErrMsg("非法输入"), "")
+	}
 
-	return &pb.GetDiscountFlightsResp{}, nil
+	var flightInfos []*commonModel.FlightInfos
+
+	// 从 bizcache 查 id 列表
+	zset := fmt.Sprintf("%s_%s", in.DepartPosition, in.ArrivePosition)
+	idList, err := bizcache.ListAll(l.svcCtx.Redis, zset, bizcache.BizFLICachePrefix)
+	// 查不到 bizcache 的情况
+	if err != nil || idList == nil {
+		flightInfos, err = l.svcCtx.FlightInfosModel.FindPageListByPositionAndDays(l.svcCtx.FlightInfosModel.RowBuilder(), in.DepartPosition, in.ArrivePosition, in.Days, -1)
+		if err != nil {
+			if err == commonModel.ErrNotFound {
+				return nil, errors.Wrapf(ERRGetInfos, "NOT FOUND: can't found flight infos: departPosition->%s arrivePosition->%v, ERR: %v\n", in.DepartPosition, in.ArrivePosition, err)
+			} else {
+				return nil, errors.Wrapf(ERRDBERR, "DBERR: when calling flightinquiry-rpc:l.svcCtx.FlightInfosModel.FindPageListByPositionAndDays : departPosition->%s arrivePosition->%v, ERR: %v\n", in.DepartPosition, in.ArrivePosition, err)
+			}
+		}
+
+		// 把id列表加进bizcache
+		for _, info := range flightInfos {
+			idList[info.Id] = struct{}{}
+			err = bizcache.AddID(l.svcCtx.Redis, info.Id, zset, bizcache.BizFLICachePrefix)
+			if err != nil {
+				logx.Errorf("ADD bizcache ERR: %v", err)
+			}
+		}
+	}
+
+	flightInfos, err = l.svcCtx.GetFlightInfosByIdList(idList)
+	if err != nil {
+		return nil, err
+	}
+
+	// 组合成完整数据组
+	combinedInfos, err := l.svcCtx.CombineAllInfos(flightInfos)
+	if err != nil {
+		return nil, err
+	}
+
+	ret := l.getDiscountFlights(combinedInfos, in.Num)
+	return &pb.GetDiscountFlightsResp{FlightInfos: ret}, nil
+}
+
+// 获取折扣最大的 n 条航班信息
+func (l *GetDiscountFlightsLogic) getDiscountFlights(flightList []*pb.FlightInfo, n int64) []*pb.FlightInfo {
+	futureFlightList := make([]*pb.FlightInfo, 0, len(flightList))
+	for i, info := range flightList {
+		if info.DepartTime.AsTime().After(time.Now()) {
+			futureFlightList[i] = info
+		}
+	}
+
+	// 降序排序
+	sort.Sort(CanBeSortedFLI(futureFlightList))
+	if n <= 0 {
+		return futureFlightList
+	}
+
+	return futureFlightList[:n]
+}
+
+func (c CanBeSortedFLI) Len() int {
+	return len(c)
+}
+
+// Less 此处用 > 来降序
+func (c CanBeSortedFLI) Less(i, j int) bool {
+	return c[i].Discount > c[j].Discount
+}
+
+func (c CanBeSortedFLI) Swap(i, j int) {
+	c[i], c[j] = c[j], c[i]
 }
