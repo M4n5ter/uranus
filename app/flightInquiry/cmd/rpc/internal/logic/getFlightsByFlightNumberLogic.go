bfdd375f0acebf4793b695f6a0b1cfa2803be608
M4n5ter <wangyongtao2000@qq.com>
2022-05-14 19:51:15 +0800

    使用 bizcache

--- app/flightInquiry/cmd/rpc/internal/logic/getFlightsByFlightNumberLogic.go
+++ app/flightInquiry/cmd/rpc/internal/logic/getFlightsByFlightNumberLogic.go
@@ -2,7 +2,9 @@ package logic
 
 import (
 	"context"
+	"fmt"
 	"github.com/pkg/errors"
+	"uranus/app/flightInquiry/bizcache"
 	"uranus/common/xerr"
 	"uranus/commonModel"
 
@@ -33,13 +35,44 @@ func (l *GetFlightsByFlightNumberLogic) GetFlightsByFlightNumber(in *pb.GetFligh
 		return nil, errors.Wrapf(xerr.NewErrMsg("非法输入"), "")
 	}
 
-	flightInfos, err := l.svcCtx.FlightInfosModel.FindPageListByNumberAndDays(l.svcCtx.FlightInfosModel.RowBuilder(), in.FlightNumber, in.Days, in.Num)
-	if err != nil && err != commonModel.ErrNotFound {
-		return nil, errors.Wrapf(xerr.NewErrCode(xerr.DB_ERROR), "DBERR : %+v", err)
+	var flightInfos []*commonModel.FlightInfos
+
+	// 从 bizcache 查 id 列表
+	zset := fmt.Sprintf("FlightNumber_%s", in.FlightNumber)
+	idList, err := bizcache.ListAll(l.svcCtx.Redis, zset, bizcache.BizFLICachePrefix)
+	if err != nil || len(idList) == 0 {
+		// 查不到 bizcache
+		flightInfos, err := l.svcCtx.FlightInfosModel.FindPageListByNumberAndDays(l.svcCtx.FlightInfosModel.RowBuilder(), in.FlightNumber, in.Days, in.Num)
+		if err != nil && err != commonModel.ErrNotFound {
+			return nil, errors.Wrapf(xerr.NewErrCode(xerr.DB_ERROR), "DBERR : %+v", err)
+		}
+
+		if flightInfos == nil {
+			return nil, errors.Wrapf(xerr.NewErrMsg("航班信息不存在"), "Err Not Found : flightNumber: %s", in.FlightNumber)
+		}
+
+		// 把 id 列表加进 bizcache
+		for _, info := range flightInfos {
+			err = bizcache.AddID(l.svcCtx.Redis, info.Id, zset, bizcache.BizFLICachePrefix)
+			if err != nil {
+				logx.Errorf("ADD bizcache ERR: %v", err)
+			}
+		}
+
+		combinedResp, err := l.svcCtx.CombineAllInfos(flightInfos)
+		if err != nil {
+			return nil, err
+		}
+
+		uniqFlightWithSpaces := l.svcCtx.GetUniqFlightWithSpacesFromCombinedFlightInfos(combinedResp)
+
+		return &pb.GetFlightsByFlightNumberResp{UniqFlightWithSpaces: uniqFlightWithSpaces}, nil
 	}
 
-	if flightInfos == nil {
-		return nil, errors.Wrapf(xerr.NewErrMsg("航班信息不存在"), "Err Not Found : flightNumber: %s", in.FlightNumber)
+	// 查到 bizcache
+	flightInfos, err = l.svcCtx.GetFlightInfosByIdList(idList)
+	if err != nil {
+		return nil, err
 	}
 
 	combinedResp, err := l.svcCtx.CombineAllInfos(flightInfos)
