43a906688e4ef29ac529cf966388db66ee29da1b
M4n5ter <wangyongtao2000@qq.com>
2022-05-18 18:16:14 +0800

    complete

--- app/flightReservation/cmd/api/internal/logic/flightReservation/flightChangeLogic.go
+++ app/flightReservation/cmd/api/internal/logic/flightReservation/flightChangeLogic.go
@@ -2,6 +2,10 @@ package flightReservation
 
 import (
 	"context"
+	"github.com/pkg/errors"
+	"uranus/app/flightReservation/cmd/rpc/flightreservation"
+	"uranus/common/ctxdata"
+	"uranus/common/xerr"
 
 	"uranus/app/flightReservation/cmd/api/internal/svc"
 	"uranus/app/flightReservation/cmd/api/internal/types"
@@ -24,6 +28,19 @@ func NewFlightChangeLogic(ctx context.Context, svcCtx *svc.ServiceContext) Fligh
 }
 
 func (l *FlightChangeLogic) FlightChange(req *types.FlightChangeReq) (resp *types.FlightChangeResp, err error) {
+	if len(req.OrderSn) == 0 || req.SpaceID < 1 {
+		return nil, errors.Wrapf(xerr.NewErrMsg("非法输入"), "")
+	}
 
-	return
+	userID := ctxdata.GetUidFromCtx(l.ctx)
+	rpcResp, err := l.svcCtx.FlightReservationRpcClient.ChangeAirTickets(l.ctx, &flightreservation.ChangeAirTicketsReq{
+		UserID:  userID,
+		OrderSn: req.OrderSn,
+		SpaceID: req.SpaceID,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return &types.FlightChangeResp{NewOrderSn: rpcResp.OrderSn}, nil
 }
