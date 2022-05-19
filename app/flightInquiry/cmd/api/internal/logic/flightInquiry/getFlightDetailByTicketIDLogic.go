16cd76469913d33d6e886b1c202f6325134451f8
M4n5ter <wangyongtao2000@qq.com>
2022-05-19 16:58:04 +0800

    complete

--- app/flightInquiry/cmd/api/internal/logic/flightInquiry/getFlightDetailByTicketIDLogic.go
+++ app/flightInquiry/cmd/api/internal/logic/flightInquiry/getFlightDetailByTicketIDLogic.go
@@ -2,6 +2,9 @@ package flightInquiry
 
 import (
 	"context"
+	"github.com/pkg/errors"
+	"uranus/app/flightInquiry/cmd/rpc/flightinquiry"
+	"uranus/common/timeTools"
 
 	"uranus/app/flightInquiry/cmd/api/internal/svc"
 	"uranus/app/flightInquiry/cmd/api/internal/types"
@@ -24,7 +27,44 @@ func NewGetFlightDetailByTicketIDLogic(ctx context.Context, svcCtx *svc.ServiceC
 }
 
 func (l *GetFlightDetailByTicketIDLogic) GetFlightDetailByTicketID(req *types.GetFlightDetailByTicketIDReq) (resp *types.GetFlightDetailByTicketIDResp, err error) {
-	// todo: add your logic here and delete this line
+	if req.TicketID < 1 {
+		return nil, errors.Wrapf(ERRIllegalInput, "")
+	}
 
-	return
+	rpcResp, err := l.svcCtx.FlightInquiryClient.FlightDetail(l.ctx, &flightinquiry.FlightDetailReq{TicketID: req.TicketID})
+	if err != nil {
+		return nil, err
+	}
+
+	resp = &types.GetFlightDetailByTicketIDResp{}
+	resp.Flightinfo = types.Flightinfo{
+		FlightInfoID:   rpcResp.FlightInfo.FlightInfoID,
+		FlightNumber:   rpcResp.FlightInfo.FlightNumber,
+		FlightType:     rpcResp.FlightInfo.FlightType,
+		SetOutDate:     timeTools.Timestamppb2TimeStringYMD(rpcResp.FlightInfo.SetOutDate),
+		IsFirstClass:   rpcResp.FlightInfo.IsFirstClass,
+		SpaceID:        rpcResp.FlightInfo.SpaceID,
+		Price:          rpcResp.FlightInfo.Price,
+		Discount:       rpcResp.FlightInfo.Discount,
+		Surplus:        rpcResp.FlightInfo.Surplus,
+		Punctuality:    rpcResp.FlightInfo.Punctuality,
+		DepartPosition: rpcResp.FlightInfo.DepartPosition,
+		DepartTime:     timeTools.Timestamppb2TimeStringYMDhms(rpcResp.FlightInfo.DepartTime),
+		ArrivePosition: rpcResp.FlightInfo.ArrivePosition,
+		ArriveTime:     timeTools.Timestamppb2TimeStringYMDhms(rpcResp.FlightInfo.ArriveTime),
+	}
+
+	ri := make([]types.TF, len(rpcResp.FlightInfo.RefundInfo.TimeFees))
+	for i, tf := range rpcResp.FlightInfo.RefundInfo.TimeFees {
+		ri[i].Fee = tf.Fee
+		ri[i].Time = timeTools.Timestamppb2TimeStringYMDhms(tf.Time)
+	}
+	ci := make([]types.TF, len(rpcResp.FlightInfo.ChangeInfo.TimeFees))
+	for i, tf := range rpcResp.FlightInfo.ChangeInfo.TimeFees {
+		ci[i].Fee = tf.Fee
+		ci[i].Time = timeTools.Timestamppb2TimeStringYMDhms(tf.Time)
+	}
+
+	resp.Flightinfo.RefundInfo.TFs, resp.Flightinfo.ChangeInfo.TFs = ri, ci
+	return resp, nil
 }
