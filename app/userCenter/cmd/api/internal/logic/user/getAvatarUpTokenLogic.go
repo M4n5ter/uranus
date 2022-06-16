2028f4cb8b316679e7fdd62db0ce3e8449971207
M4n5ter <wangyongtao2000@qq.com>
2022-06-16 09:26:25 +0800

    强制指定了用户头像 Key

--- app/userCenter/cmd/api/internal/logic/user/getAvatarUpTokenLogic.go
+++ app/userCenter/cmd/api/internal/logic/user/getAvatarUpTokenLogic.go
@@ -2,8 +2,13 @@ package user
 
 import (
 	"context"
+	"github.com/pkg/errors"
 	"github.com/qiniu/go-sdk/v7/auth/qbox"
 	"github.com/qiniu/go-sdk/v7/storage"
+	"github.com/zeromicro/go-zero/core/hash"
+	"uranus/app/userCenter/cmd/rpc/usercenter"
+	"uranus/common/ctxdata"
+	"uranus/common/xerr"
 
 	"uranus/app/userCenter/cmd/api/internal/svc"
 	"uranus/app/userCenter/cmd/api/internal/types"
@@ -26,14 +31,29 @@ func NewGetAvatarUpTokenLogic(ctx context.Context, svcCtx *svc.ServiceContext) *
 }
 
 func (l *GetAvatarUpTokenLogic) GetAvatarUpToken(req *types.GetAvatarUpTokenReq) (resp *types.GetAvatarUpTokenResp, err error) {
+	userid := ctxdata.GetUidFromCtx(l.ctx)
+	if userid == 0 {
+		return nil, errors.Wrapf(xerr.NewErrMsg("非法用户"), "用户 ID 为 0！")
+	}
+
+	userinfoResp, err := l.svcCtx.UsercenterRpcClient.GetUserInfo(l.ctx, &usercenter.GetUserInfoReq{Id: userid})
+	if err != nil {
+		return nil, err
+	}
+
+	nickname, mobile := userinfoResp.User.Nickname, userinfoResp.User.Mobile
+	key := hash.Md5Hex([]byte(nickname + "_" + mobile))
+
 	accessKey := l.svcCtx.Config.QiniuOSS.AccessKey
 	secretKey := l.svcCtx.Config.QiniuOSS.SecretKey
 	bucket := l.svcCtx.Config.QiniuOSS.Bucket
 	mac := qbox.NewMac(accessKey, secretKey)
 	putPolicy := storage.PutPolicy{
-		Scope:      bucket,
-		MimeLimit:  "image/jpeg;image/png",
-		FsizeLimit: 10_485_760,
+		Scope:        bucket,
+		MimeLimit:    "image/jpeg;image/png",
+		FsizeLimit:   10_485_760,
+		ForceSaveKey: true,
+		SaveKey:      key,
 	}
 	upToken := putPolicy.UploadToken(mac)
 
