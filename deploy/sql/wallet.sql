0ca0d1251ca8e5381fac6f1f60f891b83326bb51
M4n5ter <wangyongtao2000@qq.com>
2022-06-16 15:10:48 +0800

    去除多余换行符

--- deploy/sql/wallet.sql
+++ deploy/sql/wallet.sql
@@ -28,7 +28,7 @@ CREATE TABLE `wallet`  (
   `delete_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `del_state` tinyint NOT NULL DEFAULT 0,
   `version` bigint NOT NULL DEFAULT 0,
-  `user_id` bigint NOT NULL COMMENT '用户id\r\n',
+  `user_id` bigint NOT NULL COMMENT '用户id',
   `money` bigint NOT NULL DEFAULT 0 COMMENT '余额(分)',
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE INDEX `idx_user_id`(`user_id`) USING BTREE
